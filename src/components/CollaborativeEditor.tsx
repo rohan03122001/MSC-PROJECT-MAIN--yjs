a154259c73dd9d58b2d46228cab6d1deb1cbe5bb
Rohan Bhujbal <rohan.bhujbal@godiverse.co.uk>
2024-07-31 00:17:37 +0100

    try5

--- src/components/CollaborativeEditor.tsx
+++ src/components/CollaborativeEditor.tsx
@@ -56,7 +56,7 @@ function CollaborativeEditor({
       ydocRef.current = ydoc;
 
       const provider = new WebsocketProvider(
-        `wss://${process.env.NEXT_PUBLIC_VM_IP}:1234`,
+        `ws://138.68.141.173:1234`,
         `monaco-room-${roomId}`,
         ydoc
       );
